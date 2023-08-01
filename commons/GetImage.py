0f9b106120c0fb0885665adfa701de95d0ffafe8
r2d2tm <r2d2tm@gmail.com>
2023-08-01 17:23:26 +0200

    ajouts dossier main_menu

--- commons/GetImage.py
+++ commons/GetImage.py
@@ -2,7 +2,16 @@ import pygame
 import os
 from pygame.locals import *
 base_path = os.path.dirname(os.path.abspath(__file__))
+
+
+
+# ======================================================================
+#                    FONCTIONS COMMUNES REUTILISABLES PARTOUT
+# ======================================================================
 def get_image(file_name, widthDest, HeightDest):
     image = pygame.image.load(os.path.join(base_path, file_name))
     image = pygame.transform.scale(image, (widthDest, HeightDest))
     return image
+
+
+# ======================================================================
