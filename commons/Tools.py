d4c0c673f4193ae2ae5b1a690178463894566441
also0712 <alexandre.sorel0712@gmail.com>
2023-08-02 14:48:43 +0200

    continuation des textures

--- commons/Tools.py
+++ commons/Tools.py
@@ -9,6 +9,7 @@ base_path = os.path.dirname(os.path.abspath(__file__))
 #                    FONCTIONS COMMUNES REUTILISABLES PARTOUT
 # ======================================================================
 def get_image(file_name, widthDest, HeightDest):
+    global base_path
     image = pygame.image.load(os.path.join(base_path, file_name))
     image = pygame.transform.scale(image, (widthDest, HeightDest))
     return image
