ce968a89bbf14d8c824d1d8f41020d6891ac3bed
r2d2tm <r2d2tm@gmail.com>
2023-08-05 15:08:58 +0200

    espacement menus

--- main_menu/MenuElement.py
+++ main_menu/MenuElement.py
@@ -15,7 +15,10 @@ class MenuElement:
         self.is_selected=False
         self.color = (150,150,150) #gris
         self.color_selected = (200,200,200) #gris clair
-        self.font = pygame.font.SysFont('Comic Sans MS', 30)
+        self.font = pygame.font.SysFont('Comic Sans MS', 50)
+
+        self.surf_elt = self.font.render(self.text, True, self.color)
+        
 
 
     def draw(self, env):
@@ -28,9 +31,9 @@ class MenuElement:
         self.x_pix = env.largeur_fenetre_pxl // 2 - rect_element[2] // 2
         
         rect_element[0] = self.x_pix
-        rect_element[1] = self.y_pix
+        rect_element[1] = self.y_pix - rect_element[3]//2
         rect_element[2] += self.x_pix
-        rect_element[3] += self.y_pix
+        rect_element[3] += self.y_pix - rect_element[3] //2
         env.fenetre.blit(surf_elt, rect_element)
  
 
